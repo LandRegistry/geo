a4e91be4fef1196f6f05dcdc39aa7900852138ec
Richard Pope <richard@memespring.co.uk>
2014-08-04 10:38:37 +0100

    removed django app

--- geo/resources.py
+++
@@ -1,7 +0,0 @@
-from restless.dj import DjangoResource
-import models
-
-class Title(DjangoResource):
-
-    def list(self):
-    	return models.Title.objects.all()
--- geo/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-import resources
-
-urlpatterns = patterns('',
-    url(r'^titles', include(resources.Title.urls())),
-)
